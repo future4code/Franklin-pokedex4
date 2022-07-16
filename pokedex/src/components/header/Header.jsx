1a0a1ce371f95a088ee7f04b1a6fe911650008a6
Joely Brito <joely_brito@outlook.com>
2022-07-16 19:01:46 -0300

    adicionado botão de voltar

--- pokedex/src/components/header/Header.jsx
+++ pokedex/src/components/header/Header.jsx
@@ -2,10 +2,14 @@ import React from "react"
 import { Header, ImgPoke } from "./styled"
 import { ButtonHome} from "../../pages/initialpage/styled"
 import logo from "../../images/logo-pokemon.png";
+import { useNavigate } from "react-router-dom"
 
 function HeaderComponent() {
+    const navigate = useNavigate()
     return (
         <Header>
+            <ButtonHome
+            onClick={() => navigate("/home")}>Voltar</ButtonHome>
             <div>
                <ImgPoke src={logo}/>
             </div>
--- pokedex/src/pages/detailspage/DetailsPage.jsx
+++ pokedex/src/pages/detailspage/DetailsPage.jsx
@@ -10,6 +10,7 @@ import { ContainerData } from "./styled";
 import { Type } from "./styled";
 import { Moves } from "./styled";
 import { Progress } from "./styled";
+import HeaderComponent from "../../components/header/Header"
 
 function DetailsPage() {
   const pathParams = useParams();
@@ -29,47 +30,55 @@ function DetailsPage() {
       });
   }, []);
   return (
-    <Container>
-      <ContainerData>
-        <Images>
-          <CardImg
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pathParams.pokemonId}.png`}
-          />
-          <CardImg
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pathParams.pokemonId}.png`}
-          />
-        </Images>
-        <States>
-          <h1>States</h1>
-          {stats.map((stat) => {
-            if (stat.stat.name === "hp") {
+    <>
+    <HeaderComponent />
+      <Container>
+        <ContainerData>
+          <Images>
+            <CardImg
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pathParams.pokemonId}.png`}
+            />
+            <CardImg
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pathParams.pokemonId}.png`}
+            />
+          </Images>
+          <States>
+            <h1>States</h1>
+            {stats.map((stat) => {
+              if (stat.stat.name === "hp") {
+                return (
+                  <p>
+                    <strong>Hp:</strong>
+                    <Progress value={stat.base_stat} max="100">
+                      {stat.base_stat}
+                    </Progress>
+                  </p>
+                );
+              }
               return (
-                <p><strong>Hp:</strong><Progress value={stat.base_stat} max="100">{stat.base_stat}</Progress></p>
+                <p key={stat.stat.name}>
+                  <strong>{stat.stat.name}:</strong> {stat.base_stat}
+                </p>
               );
-            }
-            return (
-              <p key={stat.stat.name}>
-                <strong>{stat.stat.name}:</strong> {stat.base_stat}
-              </p>
-            );
-          })}
-        </States>
-        <ContainerTypeMoves>
-          <Type>
-            <h2>Types</h2>
-            {types.map((type) => {
-              return <p key={type.slot}>{type.type.name}</p>;
             })}
-          </Type>
-          <Moves>
-            <h2>Moves</h2>
-            {moves.map((move) => {
-              return <p key={move.move.name}>{move.move.name}</p>;
-            })}
-          </Moves>
-        </ContainerTypeMoves>
-      </ContainerData>
-    </Container>
+          </States>
+          <ContainerTypeMoves>
+            <Type>
+              <h2>Types</h2>
+              {types.map((type) => {
+                return <p key={type.slot}>{type.type.name}</p>;
+              })}
+            </Type>
+            <Moves>
+              <h2>Moves</h2>
+              {moves.map((move) => {
+                return <p key={move.move.name}>{move.move.name}</p>;
+              })}
+            </Moves>
+          </ContainerTypeMoves>
+        </ContainerData>
+      </Container>
+      </>
   );
 }
 export default DetailsPage;
